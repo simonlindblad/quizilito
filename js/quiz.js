393438b3f11ca13524136a465d462feed34ae6c7
Viktor Holmgren <viktor.holmgren@gmail.com>
2015-07-21 11:00:39 +0200

    First prototype of quiz, generates input fields, radio next

---
+++ js/quiz.js
@@ -0,0 +1,51 @@
+'use strict'
+
+var data;
+var inputCounter = 0;
+var radioCounter = 0;
+jQuery(document).ready(function($) {
+    $.getJSON("test-data.json", function(json) {
+        data = json;
+        generateHTML(json);
+    });
+});
+
+function generateHTML(data) {
+    console.log(data);
+    document.title = data.title;
+
+    data.questions.forEach(function(question) {
+
+        var description = "<h4>" + question.description + "</h4>";
+
+        var d1 = document.getElementById('cd-form');
+        d1.insertAdjacentHTML('beforeend', description);
+
+        if (isInputQuestion(question)) {
+            d1.insertAdjacentHTML('beforeend', generateInput());
+        }
+    });
+
+    generateSubmit();
+}
+
+function generateInput() {
+    var uniqueName = "cd-input-" + inputCounter;
+    inputCounter++;
+    return '<input class="input" name=' + uniqueName + ' type="text" required>';
+}
+
+function generateSubmit() {
+    var form = document.getElementById('cd-form');
+    form.insertAdjacentHTML('beforeend',
+        '<div><input type="submit" value="Send Message"></div>');
+}
+
+function isInputQuestion(question) {
+    return question.alternatives.length == 0;
+}
+
+$("form").submit(function(event) {
+    console.log($(this).serializeArray());
+    event.preventDefault();
+});
