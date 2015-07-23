37e9c057f394822814ac1c1318621616c2990b58
Viktor Holmgren <viktor.holmgren@gmail.com>
2015-07-23 09:29:49 +0200

    Forgot js file, fixed now

---
+++ data.js
@@ -0,0 +1,20 @@
+var data = {
+    "title": "Hejsan!",
+    "questions": [
+        {
+            "description": "What comes after 'a'?",
+            "alternatives": [],
+            "answer": "b"
+        },
+        {
+            "description": "What comes after 'b'?",
+            "alternatives": [],
+            "answer": "c"
+        },
+        {
+            "description": "What comes after 'c'?",
+            "alternatives": ["d", "e", "f"],
+            "answer": "d"
+        }
+    ]
+}
