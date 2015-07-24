59ed3d82cd9873322d9585e6a868e62b7bf46ac3
Simon Lindblad <simon.lindblad93@gmail.com>
2015-07-24 11:29:38 +0200

    UPdated

--- build/satser-uttryck.js
+++ build/satser-uttryck.js
@@ -3,9 +3,19 @@ var data = {
     "questions": [
 		{
             "description": "För vilka värden på n kommer 'Fall 2' skrivas ut?",
-			"gist-id": "529548cb1623192fbdf4",
-            "alternatives": ["n > 0", "n >= 0", "2 > n >= 0"],
+			"gist_id": "529548cb1623192fbdf4",
+            "alternatives": ["n > 2", "n >= 0", "2 > n >= 0"],
             "answer": "2 > n >= 0"
+        }, {
+            "description": "För vilka värden på n kommer 'Fall 2' skrivas ut?",
+			"gist_id": "f3711d88a35741761198",
+            "alternatives": ["n > 2", "n >= 0", "2 > n >= 0"],
+            "answer": "n >= 0"
+        }, {
+            "description": "För vilka värden på n kommer 'Fall 2' skrivas ut?",
+			"gist_id": "9a953c0475b68c47a1f4",
+            "alternatives": ["n > 2", "n >= 0", "2 > n >= 0"],
+            "answer": "n > 2"
         }
     ]
 }
