ae57507b76a287cea9264158e8604ea532d60c85
henning <henna497@student.liu.se>
2015-07-28 15:23:07 +0200

    nya uppgifter

---
+++ build/testning.js
@@ -0,0 +1,18 @@
+var data = {
+    "title": "Testning",
+    "questions": [
+        {
+            "description": "Vilken sorts testning kontrolerar att funktioner fungerar ihop som de ska?",
+            "alternatives": ["Acceptanstest", "Integrationstest", "Enhetstest", "Systemtest"],
+            "answer": "Integrationstest"
+        }, {
+            "description": "Vilken sorts test utförs av en kund?",
+            "alternatives": ["Acceptanstest", "Integrationstest", "Enhetstest", "Systemtest"],
+            "answer": "Acceptanstest"
+        }, {
+            "description": "Är assert en sats eller ett uttryck?",
+            "alternatives": ["Sats", "uttryck"],
+            "answer": "Sats"
+        }
+    ]
+}
