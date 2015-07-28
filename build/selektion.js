944d9fe45eb5d71659c7e431bc5667eed48453c6
henning <henna497@student.liu.se>
2015-07-28 09:05:30 +0200

    ny quizz

---
+++ build/selektion.js
@@ -0,0 +1,36 @@
+var data = {
+    "title": "Selektion",
+    "questions": [
+        {
+            "description": "Vilket sanningsvärde har uttrycket?",
+            "gist_id": "451614573c6cf03721cb",
+            "alternatives": ["True", "False"],
+            "answer": "True"
+        }, {
+            "description": "Vilket sanningsvärde har uttrycket?",
+            "gist_id": "27e05f7545bde0120db1",
+            "alternatives": ["True", "False"],
+            "answer": "False"
+        }, {
+            "description": "Vilket sanningsvärde har uttrycket?",
+            "gist_id": "cf2e594c448bb14b117e",
+            "alternatives": ["True", "False"],
+            "answer": "True"
+        }, {
+            "description": "Vilket sanningsvärde har uttrycket?",
+            "gist_id": "6a003c92909518665638",
+            "alternatives": ["True", "False"],
+            "answer": "False"
+        }, {
+            "description": "Vad kommer uttrycket att printa?",
+            "gist_id": "accdc2009d19de68d48b",
+            "alternatives": ["hej", "hej då", "då"],
+            "answer": "hej"
+        }, {
+            "description": "Vilket sanningsvärde har uttrycket?",
+            "gist_id": "7c91f4e21d0bb8105bbf",
+            "alternatives": ["True", "False"],
+            "answer": "False"
+        }
+    ]
+}
