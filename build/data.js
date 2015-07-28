9a934881a603ab8c7678aac6b560f0904cf44cdf
henning <henna497@student.liu.se>
2015-07-28 12:00:31 +0200

    bättre template

--- build/data.js
+++ build/data.js
@@ -1,18 +1,16 @@
 var data = {
-    "title": "Hejsan!",
+    "title": "Hejsan",
     "questions": [
         {
             "description": "What comes after 'a'?",
             "gist_id": "d6e36bb443a0aa71a99a",
             "alternatives": [],
             "answer": "b"
-        },
-        {
+        }, {
             "description": "What comes after 'b'?",
             "alternatives": [],
             "answer": "c"
-        },
-        {
+        }, {
             "description": "What comes after 'c'?",
             "alternatives": ["d", "e", "f"],
             "answer": "d"
