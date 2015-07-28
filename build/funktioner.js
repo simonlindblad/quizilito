fb5295df05ba237f77dc24bafd73a29a264fbed9
henning <henna497@student.liu.se>
2015-07-28 15:22:55 +0200

    nya uppgifter

---
+++ build/funktioner.js
@@ -0,0 +1,22 @@
+var data = {
+    "title": "Funktioner",
+    "questions": [
+        {
+            "description": "När en tupel skickas som argument till en funktion, skickas en kopia eller en referens?",
+            "alternatives": ["Kopia", "Referens"],
+            "answer": "Referens"
+        }, {
+            "description": "Kan en funktion A komma åt en lokal variabel i en annan funktion B som ej skickats som parameter?",
+            "alternatives": ["Ja", "Nej", "Endast om A är en inre funktion till B"],
+            "answer": "Endast om A är en inre funktion till B"
+        }, {
+            "description": "En destruktiv funktion är en funktion som",
+            "alternatives": ["har sido-effekter", "skär sig i handlederna", "ändrar värdet på en lista"],
+            "answer": "har sido-effekter"
+        }, {
+            "description": "En lista kan innehålla sig själv",
+            "alternatives": ["Sant", "Falskt"],
+            "answer": "Sant"
+        }
+    ]
+}
