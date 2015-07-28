ff41cd50788c8fc561cf127c29509eb978ce1997
henning <henna497@student.liu.se>
2015-07-28 10:43:44 +0200

    nya uppgifter

---
+++ build/mer_om_datatyper.js
@@ -0,0 +1,40 @@
+var data = {
+    "title": "Mer om datatyper",
+    "questions": [
+        {
+            "description": "Vilket sanningsvärde har uttrycket?",
+            "gist_id": "b87f5514070f44d36fa1",
+            "alternatives": ["True", "False"],
+            "answer": "False"
+        }, {
+            "description": "Vad returnerar uttrycket?",
+            "gist_id": "c520931a96a8d7867222",
+            "alternatives": ["1", "2", "3"],
+            "answer": "2"
+        }, {
+            "description": "Vilket av följande värden kan INTE användas som nyckel i en dictionary?",
+            "alternatives": ["14", "'Pappa'", "(['Pappa'])", "('Pappa')"],
+            "answer": "(['Pappa'])"
+        }, {
+            "description": "Listor kan användas som värde i en dictionary.",
+            "alternatives": ["Sant", "Falskt"],
+            "answer": "Sant"
+        }, {
+            "description": "I ett binärt sökträd är noden med högst värde alltid ett löv.",
+            "alternatives": ["Sant", "Falskt"],
+            "answer": "Falskt"
+        }, {
+            "description": "I ett binärt sökträd saknar noden med lägst värde vänsterbarn.",
+            "alternatives": ["Sant", "Falskt"],
+            "answer": "Sant"
+        }, {
+            "description": "I ett binärt sökträd kan rotnoden ej ha högst värde.",
+            "alternatives": ["Sant", "Falskt"],
+            "answer": "Falskt"
+        }, {
+            "description": "I ett binärt sökträd har alla noder två barn.",
+            "alternatives": ["Sant", "Falskt"],
+            "answer": "Falskt"
+        }
+    ]
+}
