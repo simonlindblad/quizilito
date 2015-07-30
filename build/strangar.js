aa46acc4835f5b74497b53df1c3bd2ec5f2aa67c
henning <henna497@student.liu.se>
2015-07-30 09:52:51 +0200

    fler frågor

--- build/strangar.js
+++ build/strangar.js
@@ -2,6 +2,26 @@ var data = {
     "title" : "Strängar",
     "questions": [
 		{
+            "description": "Vilket sanningsvärde har uttrycket?",
+			"gist_id": "b464000ab6faccd0bfeb",
+            "alternatives": ["True", "False"],
+            "answer": "False"
+        }, {
+            "description": "Vilket sanningsvärde har uttrycket?",
+			"gist_id": "61b712eb0ad6bba6ce84",
+            "alternatives": ["True", "False"],
+            "answer": "False"
+        }, {
+            "description": "Vilket sanningsvärde har uttrycket?",
+			"gist_id": "2910a3df9d3ddc50a538",
+            "alternatives": ["True", "False"],
+            "answer": "False"
+        }, {
+            "description": "Vilket sanningsvärde har uttrycket?",
+			"gist_id": "862589f76ada3531b89d",
+            "alternatives": ["True", "False"],
+            "answer": "True"
+        }, {
             "description": "Vilket uttryck returnerar strängen 'What is love?'?",
 			"gist_id": "8c75c94fda06565b1057",
             "alternatives": ["x[13]", "x[:13]", "x[13:]"],
