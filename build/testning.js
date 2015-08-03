264b6e155d3f60a008c2d45ddd18e33c7d514d59
henning <henna497@student.liu.se>
2015-08-03 10:14:33 +0200

    byte av sista frågan

--- build/testning.js
+++ build/testning.js
@@ -2,13 +2,9 @@ var data = {
     "title": "Testning",
     "questions": [
         {
-            "description": "Vilken sorts testning kontrolerar att funktioner fungerar ihop som de ska?",
-            "alternatives": ["Acceptanstest", "Integrationstest", "Enhetstest", "Systemtest"],
-            "answer": "Integrationstest"
-        }, {
-            "description": "Vilken sorts test utförs av kunden?",
-            "alternatives": ["Acceptanstest", "Integrationstest", "Enhetstest", "Systemtest"],
-            "answer": "Acceptanstest"
+            "description": "Enhetstestning testar:",
+            "alternatives": ["Interaktionen mellan funktioner", "Induviduella funktioner", "System", "Om produkten godkänns av kunden"],
+            "answer": "Induviduella funktioner"
         }, {
             "description": "Black box-tester skrivs utifrån dokumentationen till koden som ska testas",
             "alternatives": ["Sant", "Falskt"],
@@ -18,9 +14,9 @@ var data = {
             "alternatives": ["Sant", "Falskt"],
             "answer": "Sant"
         }, {
-            "description": "Är assert en sats eller ett uttryck?",
-            "alternatives": ["Sats", "Uttryck"],
-            "answer": "Sats"
+            "description": "Behöver du testa dina funktioner med indata som funktionen inte ska hantera",
+            "alternatives": ["Ja", "Nej"],
+            "answer": "Ja"
         }
     ]
 }
